c045bf7608a4428f62f4ceabb94eafc333814ffa
Lim Seok Min <lseokmin@gmail.com>
2013-03-30 12:03:38 +0800

    edit bat

--- auto.bat
+++ auto.bat
@@ -1,5 +1,6 @@
 for %%i in (C:\Malwares\*) do (
 md logs
 C:\pin\pin -t C:\GitHub\CP4101\ParanoidDetector\Debug\PinTool.dll -- %%i
+ping 1.1.1.1 -n 1 -w 10000 >NUL
 ren C:\GitHub\CP4101\logs %%~ni
 )
